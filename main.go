22e740eb1d958929b730f909a8837d7f1195aefc
Saqib Akhter <saqib.akhter@techlogix.com>
2024-07-21 02:38:30 +0500

    chore(): case for having empty string

--- main.go
+++ main.go
@@ -2,37 +2,65 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
-	fileName := flag.String("c", "", "Path to the file to read")
-	fmt.Println(*fileName)
-	flag.Parse()
+	funcFlag := os.Args[1]
+	if len(os.Args) < 3 {
+		file, err := os.Open(os.Args[1])
+		fName := strings.Split(os.Args[1], "/")
+		AbsfileName := fName[len(fName)-1]
+		if err != nil {
+			log.Fatalf("Failed to open file: %s", err)
+			os.Exit(1)
+		} else {
+			lineCount := count_lines(file)
+			defer file.Close()
+			file1, _ := os.Open(os.Args[1])
+			wordCount := count_words(file1)
+			defer file.Close()
+			file, _ := os.Open(os.Args[1])
+			byteCount := count_bytes(file)
+			defer file.Close()
+			fmt.Printf("%d %d %d %s \n", lineCount, wordCount, byteCount, AbsfileName)
 
-	// Extracting name of file
-	fName := strings.Split(*fileName, "/")
-	AbsfileName := fName[len(fName)-1]
+		}
+	} else {
+		resFlag := string(funcFlag[1])
+		fName := strings.Split(os.Args[2], "/")
+		AbsfileName := fName[len(fName)-1]
+		file, err := os.Open(os.Args[2])
+		if err != nil {
+			log.Fatalf("Failed to open file: %s", err)
+		}
+		if resFlag == "c" {
+			byteCount := count_bytes(file)
+			defer file.Close()
+			fmt.Printf("%d %s \n", byteCount, AbsfileName)
+		} else if resFlag == "l" {
+			lineCount := count_lines(file)
+			defer file.Close()
+			fmt.Printf("%d %s \n", lineCount, AbsfileName)
+		} else if resFlag == "w" {
+			wordCount := count_words(file)
+			defer file.Close()
+			fmt.Printf("%d %s \n", wordCount, AbsfileName)
+		} else if resFlag == "m" {
+			charCount := count_chars(file)
+			defer file.Close()
+			fmt.Printf("%d %s \n", charCount, AbsfileName)
 
-	if *fileName == "" {
-		fmt.Println("Please provide a file name using the -file flag.")
-		os.Exit(1)
+		} else {
+			fmt.Println("invalid flag")
+		}
 	}
-	file, err := os.Open(*fileName)
-	if err != nil {
-		log.Fatalf("Failed to open file: %s", err)
-	}
-	byteCount := count_bytes(file)
-	defer file.Close()
-	fmt.Printf("The file %s contains %d bytes.\n", AbsfileName, byteCount)
-
 }
-
-// Create a buffered reader
 
 func count_bytes(file *os.File) int {
 	reader := bufio.NewReader(file)
@@ -47,7 +75,42 @@ func count_bytes(file *os.File) int {
 		byteCount++
 	}
 	return byteCount
+}
 
-	// Print the byte count
+func count_lines(file *os.File) int {
+	lineCount := 0
+	reader := bufio.NewReader(file)
+	// Read the file line by line
+	for {
+		_, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		lineCount++
+	}
+	return lineCount
+}
 
+func count_words(file *os.File) int {
+	wordCount := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		wordCount += len(strings.Fields(scanner.Text()))
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return wordCount
+}
+
+func count_chars(file *os.File) int {
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	// Count the characters
+	charCount := utf8.RuneCount(content)
+	return charCount
 }
